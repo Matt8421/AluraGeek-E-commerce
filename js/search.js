1d403e4a5770eec36df4530fc5f4c26bbdf9e6b6
Matt <matiaslucero21884@gmail.com>
2025-01-07 20:03:09 -0300

    Modificando js

--- js/search.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".search_principal").addEventListener("submit", (evento) => {
-
-    evento.preventDefault();
-    const buscador = document.querySelector(".search").value;
-    window.location.href =`producto_buscado.html?buscar=${buscador}`;
-});
